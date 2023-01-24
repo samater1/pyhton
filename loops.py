a6a06b8dfa46704f1a767b23d478eee7ba7323a9
samater <timacade433@gmail.com>
2023-01-24 11:19:27 +0300

    added sets and loops

---
+++ loops.py
@@ -0,0 +1,8 @@
+names =['samater', 'mahad', 'mohamed']
+for name in names:
+    print(name)
+    print(f"the length of the lists is {len(names)}")
+
+name = "samater"
+for char in name:
+    print(char)
---
+++ sets.py
@@ -0,0 +1,11 @@
+# empty sets
+s =set()
+# sets added in ellements
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+s.remove(2)
+print(s)
+print(f"the set has {len(s)} of elements")
