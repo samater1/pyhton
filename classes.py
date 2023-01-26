d199b1e92f5290b1b8e3af28bf0f31ac23911925
samater <timacade433@gmail.com>
2023-01-26 10:33:06 +0300

    added extra objects to the class

--- classes.py
+++ classes.py
@@ -11,6 +11,8 @@ class Flight():
     def open_seats(self):
         return self.capacity-len(self.pasengers)
 flight = Flight(3)
+flight1 = Flight(3)
+flight2 = Flight(3)
 for person in range(10):
     person = input("name:")
     success = flight.add_passenger(person)
@@ -19,5 +21,14 @@ for person in range(10):
     else:
         print(f"no available seats {person}")
         break
+for person in range(10):
+    person = input("name:")
+    success = flight1.add_passenger(person)
+    if success:
+        print(f"added {person} to the flight successfuly")
+    else:
+        print(f"no available seats {person}")
+        break
 
 print(flight.pasengers)
+print(flight1.pasengers)
